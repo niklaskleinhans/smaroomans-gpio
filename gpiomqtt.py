1afdc2eec16e1dac4448e62d806df42c9ba676d7
Robert Gänzle <gaenzlrt@studi.informatik.uni-stuttgart.de>
2019-07-01 19:14:19 +0200

    #UNISMART-36 fix disconnects of publisher by using loop_start() also for subscriber instead of stobable threads

--- gpiomqtt.py
+++ gpiomqtt.py
@@ -16,20 +16,19 @@ class GpioMqtt():
     def __init__(self):
         self.windowButton = Button(18)
         # self.statusRGBLED = [LED(22), LED(27), LED(17)]
-        # self.statusRooms = [LED(26), LED(19), LED(13), LED(6), LED(5), LED(21), LED(20), LED(16), LED(12), LED(25)]
+        # self.statusRoomLEDs = [LED(26), LED(19), LED(13), LED(6), LED(5), LED(21), LED(20), LED(16), LED(12), LED(25)]
         self.states = {"room1": {"stateled": LED(26), "notificationrgbled": [LED(22), LED(27), LED(17)]}, 
                       "room2": {"stateled": LED(19)}, "room3": {"stateled": LED(13)}, "room4": {"stateled": LED(6)},
                       "room5": {"stateled": LED(5)}, "room6": {"stateled": LED(21)}, "room7": {"stateled": LED(20)},
                       "room8": {"stateled": LED(16)}, "room9": {"stateled": LED(12)}, "room10": {"stateled": LED(25)}}
-        # self.subscriber = Subscriber("192.168.1.230", self.statusRGBLED, self.statusRooms)
         self.subscriber = Subscriber("192.168.1.230", self.states)
         self.publisher = Publisher("192.168.1.230")
 
     def windowClosed(self):
-        self.publisher.publish("gpio/sensor/window", {"window":0})
+        self.publisher.publish("gpio/sensor/window", {"window": 0})
 
     def windowOpened(self):
-        self.publisher.publish("gpio/sensor/window", {"window":1})
+        self.publisher.publish("gpio/sensor/window", {"window": 1})
 
     def checkWindow(self):
         self.windowButton.when_pressed = self.windowClosed
@@ -43,9 +42,16 @@ class GpioMqtt():
     def stopSubscription(self):
         self.subscriber.stopSubscription()
 
+    def startPublisher(self):
+        self.publisher.startPublisher()
+
+    def stopPublisher(self):
+        self.publisher.stopPublisher()
+
 def main():
     gpiomqtt = GpioMqtt()
     gpiomqtt.startSubscription()
+    gpiomqtt.startPublisher()
     gpiomqtt.checkWindow()
 
 if __name__ == "__main__":
--- utilities/subscriber.py
+++ utilities/subscriber.py
@@ -5,19 +5,19 @@ import time
 import datetime
 
 class Subscriber():
-    # def __init__(self, host, statusRGBLED, statusRooms):
     def __init__(self, host, states):
         self.host = host
         self.client = mqtt.Client('smaaroomansgpiomqttsubscriber')
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
         self.client.connect(self.host)
-        # self.statusRGBLED = statusRGBLED
-        # self.statusRooms = statusRooms
+ 
         self.states = states
 
     def on_message_from_notificationrgbled(self, client, userdata, message):
+        topic = message.topic
         message = json.loads(message.payload.decode())
+        print("subscribe: ", topic, message)
         try:
             room = message["room"]
             rgb = message["state"]
@@ -26,17 +26,10 @@ class Subscriber():
         except Exception as e:
             print(e)
 
-    # def on_message_from_statusRGBLED(self, client, userdata, message):
-    #     message = json.loads(message.payload.decode())
-    #     try:
-    #         rgb = message["state"]
-    #         for index, pin in enumerate(rgb):
-    #             self.statusRGBLED[index].value = pin
-    #     except Exception as e:
-    #         print(e)
-
     def on_message_from_stateled(self, client, userdata, message):
+        topic = message.topic
         message = json.loads(message.payload.decode())
+        print("subscribe: ", topic, message)
         try:
             room = message["room"]
             state = message["state"]
@@ -44,15 +37,6 @@ class Subscriber():
             self.states[room]["stateled"].value = state
         except Exception as e:
             print(e)
-
-    # def on_message_from_statusRooms(self, client, userdata, message):
-    #     message = json.loads(message.payload.decode())
-    #     try:
-    #         states = message["data"]
-    #         for index, pin in enumerate(states):
-    #             self.statusRooms[index].value = pin
-    #     except Exception as e:
-    #         print(e)
 
     def on_connect(self, client, userdata, flags, rc):
         print("Subscriber Connected With Result Code ", rc)
@@ -67,21 +51,23 @@ class Subscriber():
         self.client.message_callback_add("actuator/notificationrgbled", self.on_message_from_notificationrgbled)
         self.client.message_callback_add("actuator/stateled", self.on_message_from_stateled)
 
-        while(not stopfunction()):
-            self.client.loop()
+        self.client.loop_start()
+
+        # while(not stopfunction()):
             # print("client loop reconnect")
             # self.client.reconnect()
             # self.client.reconnect_delay_set(10)
 
     def startSubscription(self):
-        # self.subscribe()
-        self.subscriptionThread = StopableThread(name="subscriptionThread", function=self.subscribe, args={})
-        self.subscriptionThread.start()
+        self.subscribe()
+        # self.subscriptionThread = StopableThread(name="subscriptionThread", function=self.subscribe, args={})
+        # self.subscriptionThread.start()
         # while True:
         #     print(self.subscriptionThread.isAlive())
         #     print(datetime.datetime.now().timestamp())
         #     time.sleep(1)
 
     def stopSubscription(self):
-        self.subscriptionThread.stop()
+        self.client.loop_stop()
+        # self.subscriptionThread.stop()
     
